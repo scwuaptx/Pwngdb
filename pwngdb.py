e48f64e141fa2ae64941f41732b8fee856970566
angelboy <scwuaptx@gmail.com>
2019-07-06 23:36:59 +0000

    change heap command to heapbase

--- pwngdb.py
+++ pwngdb.py
@@ -60,7 +60,7 @@ class PwnCmd(object):
 
         print("\033[34m" + "libc : " + "\033[37m" + hex(libcbs))
 
-    def heap(self):
+    def heapbase(self):
         """ Get heapbase """
         heapbase = getheapbase()
         if heapbase :
